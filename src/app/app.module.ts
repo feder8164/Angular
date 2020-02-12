f7a01a9316ab562400fefdcca7b0487e40fa45c6
feder8164 <sprite8164@gmail.com>
2020-02-12 19:59:58 +0100

    Wczytanie komponentu przez inny komponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TestComponent ],
+  declarations: [ AppComponent, HelloComponent, TestComponent, Test2Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
